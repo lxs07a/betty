950e0a5d7d97425a0e6de8ad5b6699ff0c126e36
Lyuda <lxs07a@acu.edu>
2019-04-19 18:11:59 +0300

    done

--- index.test.js
+++ index.test.js
@@ -5,7 +5,86 @@ import freeze from "deep-freeze";
  * Write a function to recursively update a given node and its children inside a tree structure.
  * We added tests to help you, run them by using `yarn test`.
  */
-function updateTree(tree, id, values) {}
+
+ //assuming the update pattern is "start at the given id, update 1, skip 1, update 2, skip 1, update 3, etc"
+ //if the assumption is not correct, just this portion would need correction
+function pattern(num1, num2) {
+
+  //the id is passed as a string, turning it to numeral
+  let id = parseInt(num1)
+
+  //starting empty pattern
+  let arr =[]
+
+  //starting the number of updates in each iteration of the pattern
+  const count = [0]
+
+  //go through each node
+  for (let i=1; i<=num2; i++) {
+    for (let j=0; j<count.length; j++) {
+
+      //while the ocunt is 0, update the first node: equal to id
+      if (i===(id+count[j])) {
+        arr.push(i)
+
+        //if this is the last update in the iteration, increase the update count
+        if (j===count.length-1) {
+          count.push(count.length)
+
+          //skip one node
+          id=i+2
+          continue  
+        }
+      }
+    }
+  }
+  return arr
+}
+
+function updateTree(tree, id, values) {
+
+  //make a copy of the deeply frozen object
+  var treeCopy = JSON.parse( JSON.stringify( tree ) );
+
+  // Retrieve the property names of the tree object
+  const objectPropNames = Object.getOwnPropertyNames(treeCopy);
+
+  //Retrieve the names of properties to be updated
+  const passedPropNames = Object.getOwnPropertyNames(values);
+
+  //Retrieve the update pattern
+  const myPattern = pattern(id, objectPropNames.length)
+  
+  //Update properties
+  //for each node
+  for (let name of objectPropNames) {
+
+    //if the node belongs to the update pattern
+    if (myPattern.includes(parseInt(treeCopy[name].id))) {
+
+      //get the list of property keys 
+      let innerPropNames = Object.getOwnPropertyNames(treeCopy[name])
+
+      //for each key
+      for (let name2 of innerPropNames) {
+
+        //for each desirable update 
+        for (let name3 of passedPropNames) {
+
+          //check if the key is up for update
+          if (name2 === name3) {
+
+            //update
+            treeCopy[name][name2] = values[name3]
+          }
+        }
+      }
+      
+    }
+  }
+  return treeCopy;
+}
+
 
 test("update child nodes", t => {
   const tree = freeze({
